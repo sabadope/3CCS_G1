e68c8804c4184f5bcf6554bd11878b461d9b9a73
FERTONY <sabado.f.bscs@gmail.com>
2025-04-01 17:55:57 +0800

    Landing Page

---
+++ TeacherParent/Pages/extra/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeacherParent.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+        // Code for handling page logic
+    }
+}
