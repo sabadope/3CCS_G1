a95b17a82109042d65a799e458c1853258f00282
Kei Kusanagi <96429812+RivenGodz@users.noreply.github.com>
2025-04-11 09:46:34 +0800

    Add files via upload

---
+++ NotificationUI/Checksched.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Notifications.Views.Home
+{
+    public class CheckschedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
