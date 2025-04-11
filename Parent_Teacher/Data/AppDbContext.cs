84756654a832baa6ac04b4c0ded64dcfc9e85137
unknown <sabado.f.bscs@gmail.com>
2025-04-11 16:16:05 +0800

    5% Working Login and Registration w/ Role Dashboard

---
+++ Parent_Teacher/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parent_Teacher.Models;
+
+namespace Parent_Teacher.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Parent_Teacher/Program.cs
@@ -0,0 +1,33 @@
+﻿using Parent_Teacher.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSession();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession(); //  Correct position
+app.UseAuthorization();
+
+app.MapRazorPages(); // Should come after UseSession
+
+app.Run();
