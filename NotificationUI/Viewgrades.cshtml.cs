289be6b34ef8ea7739c97e0914f711d0697d59bb
unknown <sabado.f.bscs@gmail.com>
2025-04-11 13:27:35 +0800

    1%-- Back to Zero

---
+++ NotificationUI/Viewgrades.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Notifications.Views.Home
+{
+    public class ViewgradesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
