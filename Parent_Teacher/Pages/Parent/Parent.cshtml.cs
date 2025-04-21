ff9faf18f022c024443365ed934c007299b73538
unknown <sabado.f.bscs@gmail.com>
2025-04-21 21:17:11 +0800

    23%-- CHAT MESSAGE

---
+++ Parent_Teacher/Pages/Parent/Parent.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Parent_Teacher.Pages.Parent
+{
+    public class ParentModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            var role = HttpContext.Session.GetString("UserRole");
+
+            if (role != "Parent")
+            {
+                return RedirectToPage("/Account/Login");
+            }
+
+            return Page();
+        }
+    }
+}
